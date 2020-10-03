d3f0cc43e752b5244d81804c9b54cf783b5416a5
Theodor <hennings.theodor@gmail.com>
2020-10-03 15:19:43 +0200

    Roterande planeter

--- LudumDare47/Assets/Scripts/Planets/PlanetRotation.cs
+++ LudumDare47/Assets/Scripts/Planets/PlanetRotation.cs
@@ -9,6 +9,6 @@ public class PlanetRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+		transform.Rotate(Vector3.forward * -rotationSpeed * Time.deltaTime);
     }
 }
