eb274d2bdebb3f5bb68b944e32c5ab45d34d4947
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2020-10-03 15:38:22 +0200

    Ship Controll

---
+++ LudumDare47/Assets/GravityController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    public float gravity;
+    public Transform gravityCenter;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(speed * 0.01f, 0, 0);
+        GetComponent<Rigidbody>().velocity += gravity * Time.fixedTime * (gravityCenter.position - transform.position);
+    }
+}
