6dcfa127f1c887f8e69d76b15e8507bddf28b6d5
oxxar <40995384+oxxar@users.noreply.github.com>
2020-10-03 17:09:04 +0200

    CrashScripts

---
+++ LudumDare47/Assets/Scripts/Player/CrashScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashScript : MonoBehaviour
+{
+    GameObject gameObject;
+
+    void Crash()
+    {
+        Debug.Log("You crashed!");
+        Destroy(gameObject);
+    }
+}
