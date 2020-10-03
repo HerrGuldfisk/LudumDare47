40a21f70093e6dbc76d864b9c43ad5e64dbf3ae4
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2020-10-03 15:28:45 +0200

    ship start

---
+++ LudumDare47/Assets/ShipController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ShipController : MonoBehaviour
+{
+    private moveForward speedScript;
+    [SerializeField] float accAmount = 1;
+
+    public InputAction gas;
+
+    private void OnEnable()
+    {
+        gas.Enable();
+    }
+    private void OnDisable()
+    {
+        gas.Disable();
+    }
+
+    private void Awake()
+    {
+        speedScript = transform.GetComponent<moveForward>();
+    }
+
+    private void Update()
+    {
+        speedScript.ChangeSpeed(gas.ReadValue<float>() * accAmount * Time.deltaTime);
+    }
+
+}
