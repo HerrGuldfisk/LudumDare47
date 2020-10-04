e17031f0fcd0620594e767ed717110a62c381b4f
Theodor <hennings.theodor@gmail.com>
2020-10-04 19:24:18 +0200

    test

---
+++ LudumDare47/Assets/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+	Camera cam;
+	Vector3 previousPos;
+	public float factor = 0.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		cam = Camera.main;
+		previousPos = cam.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position += (cam.transform.position - previousPos) * factor;
+		previousPos = cam.transform.position;
+    }
+}
