27899bac9dcc209397aacc70dd44a6a07437e511
douglasdriving <41374220+douglasdriving@users.noreply.github.com>
2020-10-05 20:35:07 +0200

    Fuel Warning System

--- LudumDare47/Assets/Scripts/Player/FuelSystem.cs
+++ LudumDare47/Assets/Scripts/Player/FuelSystem.cs
@@ -6,28 +6,31 @@ using UnityEngine.UI;
 public class FuelSystem : MonoBehaviour
 {
     [SerializeField] float standbyDepletion = 1;
+    [SerializeField] float maxFuel;
+
     private Slider fuelSlider;
     private Text screenText;
-    public bool fuelEmpty = false;
+    [HideInInspector] public bool fuelEmpty = false;
+
+    //fuel warning system
+    [SerializeField] Image fuelBorder;
+    [SerializeField] Text fuelWarningText;
+    [SerializeField] float fuelWarningFreq = 0.5f;
+    private float fuelWarningTimer;
+    private bool fuelLow = false;
 
     private void Awake()
     {
-        GameManager.currentFuel = GameManager.maxFuel;
+        GameManager.maxFuel = maxFuel;
+        GameManager.currentFuel = maxFuel;
         fuelSlider = GameObject.FindGameObjectWithTag("fuelBar").transform.GetComponent<Slider>();
         screenText = GameObject.FindGameObjectWithTag("ScreenMessage").GetComponent<Text>();
+        fuelWarningText.enabled = false;
+        fuelWarningTimer = fuelWarningFreq;
     }
 
     void Update()
-    {
-        /*
-        if (GameManager.inOrbit)
-        {
-            GameManager.currentFuel -= standbyDepletion * Time.deltaTime;
-        }
-        */
-        
-        updateSlider();
-
+    {   
         if (GameManager.currentFuel < 0)
         {
             GameManager.currentFuel = 0;
@@ -37,11 +40,29 @@ public class FuelSystem : MonoBehaviour
         {
             FuelIsEmpty();
         }
+
+        if (fuelLow)
+        {
+            fuelWarningTimer -= Time.deltaTime;
+            if (fuelWarningTimer < 0)
+            {
+                fuelWarningTimer = fuelWarningFreq;
+                fuelWarningText.enabled = !fuelWarningText.enabled;
+            }
+        }
     }
 
     public void DepleteFuel(float amount)
     {
         GameManager.currentFuel -= amount;
+        updateSlider();
+
+        if (!fuelLow && GameManager.currentFuel / GameManager.maxFuel < 0.2f)
+        {
+            fuelLow = true;
+            fuelBorder.color = Color.red;
+            fuelWarningText.enabled = true;
+        }
     }
 
     public void addFuel(float amount)
@@ -50,6 +71,14 @@ public class FuelSystem : MonoBehaviour
         GameManager.currentFuel = Mathf.Clamp(GameManager.currentFuel, 0, GameManager.maxFuel);
         fuelEmpty = false;
         screenText.enabled = false;
+        updateSlider();
+
+        if (fuelLow && GameManager.currentFuel / GameManager.maxFuel > 0.2f)
+        {
+            fuelLow = false;
+            fuelBorder.color = Color.black;
+            fuelWarningText.enabled = false;
+        }
     }
 
     private void updateSlider()
@@ -60,9 +89,5 @@ public class FuelSystem : MonoBehaviour
     private void FuelIsEmpty()
     {
         fuelEmpty = true;
-
-        screenText.enabled = true;
-        screenText.text = "Out of fuel";
-        screenText.color = Color.red;
     }
 }
